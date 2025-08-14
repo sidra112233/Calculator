141be7a090954ed2f907aca0dc850261c7ab2e4a
Sidra <candlelight081@gmail.com>
2025-08-14 16:57:26 +0500

    new

--- postcss.config.js
+++ postcss.config.js
@@ -1,15 +1,6 @@
-<<<<<<< HEAD
 export default {
   plugins: {
     "@tailwindcss/postcss": {},
     autoprefixer: {},
   },
 }
-=======
-export default {
-  plugins: {
-    "@tailwindcss/postcss": {},
-    autoprefixer: {},
-  },
-}
->>>>>>> 6f25bd7c08e516c55304969407e9868efe6873cb
